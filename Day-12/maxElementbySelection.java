3781002d8e23398fda3a8dccc22ff94af9742c31
Saravanakumar <saravanakumar182005@gmail.com>
2026-07-06 19:33:34 +0530

    Add count tracking for swaps in maxElementbySelection class

--- Day-12/maxElementbySelection.java
+++ Day-12/maxElementbySelection.java
@@ -1,6 +1,7 @@
 public class maxElementbySelection {
     public static void main(String[] args){
         int arr[]={1,2,4,3,5};
+        int count=0;
         for(int i=0;i<arr.length-1;i++){
             int MaxIndex=i;
             for(int j=i+1;j<arr.length;j++){
@@ -10,11 +11,13 @@ public class maxElementbySelection {
                 int temp=arr[MaxIndex];
                 arr[MaxIndex]=arr[i];
                 arr[i]=temp;
+                count++;
             }
          }
         for(int sum:arr){
           System.out.println(sum + " ");
         }
+        System.out.println(count);
     }
 }
 
