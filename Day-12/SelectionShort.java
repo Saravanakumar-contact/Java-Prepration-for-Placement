bfc5b885564974c3c73823fe16598411e8d1a4a8
Saravanakumar <saravanakumar182005@gmail.com>
2026-07-06 19:34:52 +0530

    Refactor SelectionSort class to include swap count tracking and improve code readability

--- Day-12/SelectionShort.java
+++ Day-12/SelectionShort.java
@@ -1,19 +1,31 @@
-public class SelectionShort {
-   public static void main(String[] args) {
-     int[] arr={64, 25, 12, 22, 11};
-     for(int i=0;i<arr.length-1;i++){
-        int minIndex=i;
-        for(int j=i+1;j<arr.length;j++){
-            if(arr[j]<arr[minIndex]){
-                minIndex=j;
-            }}
-        int temp=arr[minIndex];
-        arr[minIndex]=arr[i];
-        arr[i]=temp;
-        
-     }
-     for(int num:arr){
-        System.out.println(num +" ");
-     }
-   } 
+public class SelectionSort {
+    public static void main(String[] args) {
+
+        int[] arr = {64, 25, 12, 22, 11};
+        int swapCount = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {   // Swap only if needed
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+                swapCount++;
+            }
+        }
+
+        System.out.println("Sorted Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println("\nTotal Swaps = " + swapCount);
+    }
 }
